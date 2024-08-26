c539f50629cb278f91fbc83c18e3b5b656b79a58
Rahul <p.rahul8292@gmail.com>
2024-08-26 14:28:14 +0530

    first commit

---
+++ javapoint/src/main/java/com/javatpoint/IProductService.java
@@ -0,0 +1,10 @@
+package com.javatpoint;
+
+import java.util.List;
+
+import java.util.List;
+public interface IProductService
+{
+    List<Product> findAll();
+}
+
---
+++ javapoint/src/main/java/com/javatpoint/springBootRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springBootRestExampleApplication.class, args);
+	}
+
+}
